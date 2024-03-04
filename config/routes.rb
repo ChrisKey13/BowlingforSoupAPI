823637c8af86d5caf74f02a51c534e9181b85640
Christophe Bouquet <christophe.productowner@gmail.com>
2024-03-04 18:11:38 +0100

    feat(game-session): add endpoint to determine session winner

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   
   resources :game_sessions, only: [:create, :show] do
+    member do
+      get 'winner'
+    end
     resources :games, only: [:create, :show] do
       member do
         patch 'roll'
       end
     end
-
     resources :players, only: [:index, :create]
   end
 
